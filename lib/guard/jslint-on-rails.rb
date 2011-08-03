8280b1a2fd654f494c54c4da9dd5e4ebc659f902
Ryan Sonnek <ryan@socialcast.com>
2011-08-03 11:54:05 -0500

    remove exception

--- lib/guard/jslint-on-rails.rb
+++ lib/guard/jslint-on-rails.rb
@@ -1,55 +1,12 @@
 require 'guard'
 require 'guard/guard'
 
-puts 'loading'
 module Guard
   class JslintOnRails < Guard
-    VERSION = '0.0.2'
-
-    # def initialize(watchers = [], options = {})
-    #   @process = nil
-    #   @pid = nil
-    #   @command = options[:command]
-    #   @env = options[:env] 
-    #   @name = options[:name]
-    #   @stop_signal = options[:stop_signal] || "TERM"
-    #   super
-    # end
-    # def process_running?
-    #   begin
-    #     @pid ? ::Process.kill(0, @pid) : false
-    #   rescue Errno::ESRCH => e
-    #     false
-    #   end
-    # end
-    # def start
-    #   UI.info("Starting process #{@name}")
-    #   @process = @env ? IO.popen([@env, @command]) : IO.popen(@command)
-    #   UI.info("Started process #{@name}")
-    #   @pid = @process.pid
-    # end
-    # def stop
-    #   if @process
-    #     UI.info("Stopping process #{@name}")
-    #     ::Process.kill(@stop_signal, @process.pid)
-    #     ::Process.waitpid(@pid) rescue Errno::ESRCH
-    #     @process.close
-    #     @pid = nil
-    #     UI.info("Stopped process #{@name}")
-    #   end
-    # end
-    # def reload
-    #   stop
-    #   start
-    # end
-    # def run_all
-    #   true
-    # end
+    VERSION = '0.0.3'
 
     def run_on_change(paths)
-      puts "Running JSLint:"
-      result = system "rake jslint paths=#{paths.join(',')}"
-      raise 'JSLint Failure' unless result
+      system "rake jslint paths=#{paths.join(',')}"
     end
   end
 end
