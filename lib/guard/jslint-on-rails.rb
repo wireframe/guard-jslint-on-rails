a09ee89d00bb8d8c4561c5f35a08447562d257ef
Ryan Sonnek <ryan@socialcast.com>
2011-08-03 12:21:08 -0500

    use guard notifier

--- lib/guard/jslint-on-rails.rb
+++ lib/guard/jslint-on-rails.rb
@@ -1,19 +1,26 @@
 require 'guard'
 require 'guard/guard'
+require 'guard/notifier'
 require 'jslint'
 
 module Guard
   class JslintOnRails < Guard
-    VERSION = '0.0.4'
+    VERSION = '0.0.5'
 
     def run_on_change(paths)
-      capture_output do
-        lint = JSLint::Lint.new(
-          :paths => paths,
-          :config_path => File.join(Dir.pwd, 'config', 'jslint.yml')
-        )
-        lint.run #raises LintCheckFailure if failure
+      error = nil
+      begin
+        output = capture_output do
+          lint = JSLint::Lint.new(
+            :paths => paths,
+            :config_path => File.join(Dir.pwd, 'config', 'jslint.yml')
+          )
+          lint.run
+        end
+      rescue JSLint::LintCheckFailure => e
+        error = e
       end
+      Notifier.notify((error ? 'failed' : 'passed'), :title => 'JSLint results', :image => (error ? :failed : :success))
       true
     end
 
