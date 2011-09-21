9161e5e496607be769c473b0e08b0e577692f410
Ryan Sonnek <ryan@socialcast.com>
2011-09-21 15:02:38 -0500

    add explicit reference to top level jslint module

--- lib/guard/jslint-on-rails.rb
+++ lib/guard/jslint-on-rails.rb
@@ -5,19 +5,19 @@ require 'jslint'
 
 module Guard
   class JslintOnRails < Guard
-    VERSION = '0.0.7'
+    VERSION = '0.0.8'
 
     def run_on_change(paths)
       error = nil
       begin
         output = capture_output do
-          lint = JSLint::Lint.new(
+          lint = ::JSLint::Lint.new(
             :paths => paths,
             :config_path => File.join(Dir.pwd, 'config', 'jslint.yml')
           )
           lint.run
         end
-      rescue JSLint::LintCheckFailure => e
+      rescue ::JSLint::LintCheckFailure => e
         error = e
       end
       Notifier.notify((error ? 'failed' : 'passed'), :title => 'JSLint results', :image => (error ? :failed : :success))
