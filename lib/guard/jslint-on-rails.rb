270fe28b03cb94c10d6e1f0cbf17c407c2243762
Ryan Sonnek <ryan.sonnek@gmail.com>
2012-04-13 22:49:32 -0500

    fix path to jshint.yml config file

--- lib/guard/jslint-on-rails.rb
+++ lib/guard/jslint-on-rails.rb
@@ -9,7 +9,7 @@ module Guard
 
     def initialize(watchers=[], options={})
       super
-      @config_path = File.join(Dir.pwd, options[:config_path] || 'config/jslint.yml')
+      @config_path = File.join(Dir.pwd, options[:config_path] || 'config/jshint.yml')
     end
 
     def start
